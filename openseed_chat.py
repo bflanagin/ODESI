d2a2ac138985d77842c5a61c6e48e334980651c5
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-03-29 10:45:01 -0500

    moving index.py chat function to new standard found in the socket code

--- openseed_chat.py
+++ openseed_chat.py
@@ -151,7 +151,7 @@ def get_chat_history(userid,room,count,last):
 			database = "openseed"
 		)
 		mysearch = openseed.cursor()
-		search = "SELECT Id,record,attendees,date,speaker FROM chat WHERE room = %s AND Id > %s ORDER BY Id DESC LIMIT "+count+"
+		search = "SELECT Id,record,attendees,date,speaker FROM chat WHERE room = %s AND Id > %s ORDER BY Id DESC LIMIT "+count
 		val1 = (room,str(last))
 		mysearch.execute(search,val1)
 		result1 = mysearch.fetchall()
