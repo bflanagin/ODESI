82b6e31ab7a05a6a48ec10a68d2e45f7d6c51142
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-02 21:39:43 -0500

    working on decoupling scripts

---
+++ openseed_apptools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+sys.path.append("..")
+import mysql.connector
+import openseed_setup as Settings
+import openseed_seedgenerator as Seed
+settings = Settings.get_settings()
+
+def get_appdata(mode,appID,data):
+	openseed = mysql.connector.connect(
+		host = "localhost",
+		user = settings["dbuser"],
+		password = settings["dbpassword"],
+		database = "openseed"
+		)
+	search = openseed.cursor()
+
+	openseed.close()
+
+	return
+
+def set_appdata(mode,appID,data,update):
+
+	return
+
+def get_webapp_token(dpub,apub,verification):
+	
+	temptoken = Seed.generate_usertoken(dpub+apub)
+	
+
+	return temptoken
