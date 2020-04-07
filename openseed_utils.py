71e8f26d38c2d8471e52e6ba5ebec32add54dde4
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-07 12:55:44 -0500

    added nicer ouput to encryption test

--- openseed_utils.py
+++ openseed_utils.py
@@ -322,11 +322,11 @@ def keytest(message):
 	print("attempts blanks are good")
 	for test in result:
 		decrypted = Seed.simp_decrypt(test[0],encrypted)
-		print("attempt:"decrypted)
+		print("attempt: "+decrypted)
 		if decrypted == message:
 			print("success - which is bad")
 
-	print("using a key create right after the first"+Seed.simp_decrypt(key2,encrypted))
+	print("using a key create right after the first "+Seed.simp_decrypt(key2,encrypted))
 
 	
 	
