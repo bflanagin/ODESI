65087751c6aad60ca47f7cdfaaf27e003c2f6b1e
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-10 16:56:56 -0500

    upload utility work

---
+++ openseed_uploader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import os
+sys.path.append("..")
+import mysql.connector
+import socketserver
+import openseed_account as Account
+import openseed_seedgenerator as Seed
+#import steem_get as Get
+#import steem_submit as Submit
+#import leaderboard as LeaderBoard
+import openseed_music as Music
+import openseed_setup as Settings
+import json
+import time
+from bottle import route, run, template
+
+settings = Settings.get_settings()
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8688)
