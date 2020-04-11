13557d51ab7b6d6f93c092c99acc6c6b9984a3a1
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-11 18:27:55 -0500

    renamed openseed_uploader

--- openseed_files.py
+++ openseed_files.py
@@ -37,14 +37,15 @@ def upload():
 
 @route('/upload', method='POST')
 def do_upload():
-	category = request.forms.get('category')
-	upload = request.files.get('file')
-   	
-	#name, ext = os.path.splitext(upload.filename)
+	#category = request.forms.get('category')
+	#upload = request.files.get('file')
+   	category = request.POST['category']
+	upload = request.POST['file']
+	name, ext = os.path.splitext(upload.filename)
 
 	save_path = get_save_path_for_category(category)
-	#upload.save(save_path) # appends upload.filename automatically
-	return '<b>'+save_path+' '+str(upload)+' OK</b>' 
+	upload.save(save_path) # appends upload.filename automatically
+	return '<b>'+save_path+' '+name+' OK</b>' 
 
 def get_save_path_for_category(category):
 	path = ""
