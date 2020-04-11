5073440941b92b9ae86a745a69db951af356982a
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-11 11:29:43 -0500

    More file work

--- openseed_files.py
+++ openseed_files.py
@@ -24,7 +24,7 @@ settings = Settings.get_settings()
 @route('/')
 
 def index():
-	return template('<b>You\'ll need to supply a file name or function </b>!')
+	return template('<b>Hello {{name}}</b>!', name="you need to supply a command")
 
 def do_upload():
 	category = request.forms.get('category')
