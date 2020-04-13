e66bd51315b249a4662363b0d5ffa1ba5093edaa
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-13 11:49:56 -0500

    profile update

--- openseed_connections.py
+++ openseed_connections.py
@@ -35,14 +35,14 @@ def get_hive_connections(account):
 		for ing in watching:
 			if er == ing:
 				hiveaccount = HiveGet.get_account(er)
-				print(hiveaccount)
-				data1 = '{"name":"'+hiveaccount["profile"]["name"]+'","email":"","phone":"","profession":"","company":""}'
-				data2 = '{"about":"'+hiveaccount["profile"]["about"]+'","profile_img":"'+hiveaccount["profile"]["profile_image"]+'","banner":"'+hiveaccount["profile"]["cover_image"]+'"}'
-				blank_p = '"profile":{"openseed":'+data1+',"extended":'+data2+',"appdata":{},"misc":{},"imports":{}}'
-				#if connections == "":
-				connections.append('{"username":"'+er+'","linked":"1",'+blank_p+'}')
-				#else:
-				#	connections +=',{"name":"'+er+'","linked":"2",'+blank_p+'}'
+				if hiveaccount["profile"] != "Not found":
+					data1 = '{"name":"'+hiveaccount["profile"]["name"]+'","email":"","phone":"","profession":"","company":""}'
+					data2 = '{"about":"'+hiveaccount["profile"]["about"]+'","profile_img":"'+hiveaccount["profile"]["profile_image"]+'","banner":"'+hiveaccount["profile"]["cover_image"]+'"}'
+					blank_p = '"profile":{"openseed":'+data1+',"extended":'+data2+',"appdata":{},"misc":{},"imports":{}}'
+					#if connections == "":
+					connections.append('{"username":"'+er+'","linked":"1",'+blank_p+'}')
+					#else:
+					#	connections +=',{"name":"'+er+'","linked":"2",'+blank_p+'}'
 
 	return(connections)
 
