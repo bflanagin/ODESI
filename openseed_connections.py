bbeeed694424792a7fa853af504da0cdb98063d3
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-13 21:58:07 -0500

    profile update

--- openseed_connections.py
+++ openseed_connections.py
@@ -50,7 +50,7 @@ def get_hive_connections(account):
 					if "cover_image" in hiveaccount["profile"]:
 						theBannerImg = hiveaccount["profile"]["cover_image"]
 
-					data1 = '{"name":"'+theName+'","email":"","phone":"","profession":"","company":""}'
+					data1 = '{"name":"'+er+'","email":"","phone":"","profession":"","company":""}'
 					data2 = '{"about":"","profile_img":"'+theProfileImg+'","banner":"'+theBannerImg+'"}'
 					blank_p = '"profile":{"openseed":'+data1+',"extended":{},"appdata":{},"misc":{},"imports":{}}'
 					connections.append('{"username":"'+er+'","linked":"1",'+blank_p+'}')
