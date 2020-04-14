fceea4de38fdae7471dccaef2f1a3f9f36dea04d
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2020-04-13 22:26:13 -0500

    profile update

--- openseed_connections.py
+++ openseed_connections.py
@@ -81,18 +81,18 @@ def get_openseed_connections(account,external = True):
 		for u in exists1:
 			cname = str(u[0])
 			if accounts == "":
-				accounts = '{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile_lite(str(cname)))+'}'
+				accounts = '{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile(str(cname)))+'}'
 			else:
-				accounts = accounts+',{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile_lite(str(cname)))+'}'
+				accounts = accounts+',{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile(str(cname)))+'}'
 
 	if len(exists2) != 0:
 		ac += len(exists2)
 		for u in exists2:
 			cname = str(u[0])
 			if accounts == "":
-				accounts = '{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile_lite(str(cname)))+'}'
+				accounts = '{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile(str(cname)))+'}'
 			else:
-				accounts = accounts+',{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile_lite(str(cname)))+'}'
+				accounts = accounts+',{"username":"'+str(cname)+'","linked":"'+str(u[1])+'",'+str(user_profile(str(cname)))+'}'
 	if external == False:
 		connections = '{"total":'+str(ac)+',"connections":['+accounts.replace("'","\'")+']}'
 	else:
