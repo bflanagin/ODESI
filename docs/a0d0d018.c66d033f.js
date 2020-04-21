a7c47cae53f85d9fb5480062856b0a712c979403
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-20 22:28:49 -0500

    More documentation work

---
+++ docs/a0d0d018.c66d033f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{129:function(a){a.exports=JSON.parse('{"allTagsPath":"/OpenSeed/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/OpenSeed/blog/tags/hola"}')}}]);
