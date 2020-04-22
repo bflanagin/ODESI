118fef69ee18bcf18e8aaacba62d6c56199858ae
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-22 12:30:26 -0500

    formating output in hive_external_acctions

--- hive_external_actions.py
+++ hive_external_actions.py
@@ -8,38 +8,45 @@ sys.path.append("..")
 import openseed_setup as Settings
 
 
-def hive_store_key(account,key):
+def store_key(account,key):
 
 	process = subprocess.Popen(['hivepy', 'addkey', key], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
+	print(stdout)
+
+def import_account(acount,masterpass):
+	process = subprocess.Popen(['echo',masterpass,'|','hivepy', 'importaccount', account], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	process.wait()
+	stdout, stderr = process.communicate()
+	print(stdout)
 	
-def hive_flush_keys(account):
+def flush_keys(account):
 	keys = find_keys_by_accountname(account)
 	for key in keys:
-		process = subprocess.Popen(['hivepy', 'delkey', key], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		process = subprocess.Popen(['echo','Y','|','hivepy', 'delkey', key], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
 	print(stdout)
 
-def hive_allow_app(account,app,allowed):
+def allow_app(account,app,allowed):
 	if allowed == "posting" or allowed == "active":
 		process = subprocess.Popen(['hivepy', 'allow', '--account', account,'--permission',allowed,app], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 		process.wait()
 		stdout, stderr = process.communicate()
 	
-def hive_disallow_app(account,app,remove):
+def disallow_app(account,app,remove):
 	if remove == "posting" or remove == "active":
 		process = subprocess.Popen(['hivepy', 'disallow','--account', account,'--permission',remove,app], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 		process.wait()
 		stdout, stderr = process.communicate()
 	
-def hive_verify_user(account,postingkey):
+def verify_user(account,postingkey):
 	process = subprocess.Popen(['hivepy', 'pin', 'ls', '--type', 'recursive', thehash], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
 
-def hive_openseed_interconnect(account,postkey):
+def openseed_interconnect(account,postkey):
 
 	process = subprocess.Popen(['hivepy', 'pin', 'ls', '--type', 'recursive', thehash], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
