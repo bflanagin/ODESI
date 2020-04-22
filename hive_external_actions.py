aed40a779dd38454675065364e2b3edbff7a0bb5
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-22 11:40:57 -0500

    reworking hive commands

--- hive_external_actions.py
+++ hive_external_actions.py
@@ -14,10 +14,12 @@ def hive_store_key(account,key):
 	stdout, stderr = process.communicate()
 	
 def hive_flush_keys(account):
-
-	process = subprocess.Popen(['hivepy', 'pin', 'ls', '--type', 'recursive', thehash], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	keys = find_keys_by_accountname(account)
+	for key in keys:
+		process = subprocess.Popen(['hivepy', 'delkey', key], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
+	print(stdout)
 
 def hive_allow_app(account,app,allowed):
 
@@ -32,7 +34,6 @@ def hive_disallow_app(account,app,remove):
 	stdout, stderr = process.communicate()
 	
 def hive_verify_user(account,postingkey):
-
 	process = subprocess.Popen(['hivepy', 'pin', 'ls', '--type', 'recursive', thehash], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
@@ -42,3 +43,14 @@ def hive_openseed_interconnect(account,postkey):
 	process = subprocess.Popen(['hivepy', 'pin', 'ls', '--type', 'recursive', thehash], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	process.wait()
 	stdout, stderr = process.communicate()
+	
+def find_keys_by_accountname(account):
+	process = subprocess.Popen(['hivepy', 'listaccounts',], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	process.wait()
+	stdout, stderr = process.communicate()
+	keys = []
+	for line in stdout.readlines():
+		if line.find("|") != -1 and line.find(account) != -1:
+			keys.append(line.split("|")[3].strip())
+	return keys
+
