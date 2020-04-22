ad6f325cbe25d062521a80eaa14f15c70cdd93d5
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-21 20:51:12 -0500

    got rid of hive_submit for now

--- openseed_socket.py
+++ openseed_socket.py
@@ -6,7 +6,8 @@ import mysql.connector
 import socketserver
 import openseed_account as Account
 import hive_get as Get
-import hive_submit as Submit
+#import hive_submit as Submit
+import hive_external_actions as Submit
 import leaderboard as LeaderBoard
 import openseed_music as Music
 import openseed_connections as Connections
