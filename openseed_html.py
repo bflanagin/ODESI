c9ab2ac0715161a167640e08d8b17afc644435e6
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-23 07:17:24 -0500

    removed unused files

--- openseed_html.py
+++ openseed_html.py
@@ -4,17 +4,6 @@ import subprocess
 import sys
 import os
 sys.path.append("..")
-import mysql.connector
-import socketserver
-import openseed_account as Account
-import openseed_seedgenerator as Seed
-import openseed_utils as Utils
-import hive_get as Get
-#import hive_submit as Submit
-#import leaderboard as LeaderBoard
-import hive_external_actions as Submit
-import openseed_music as Music
-import openseed_setup as Settings
 import json
 import time
 import openseed_core as Core
