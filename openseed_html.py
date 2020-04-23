3bcfaf8b5b017fc37dcea5f9007222b9d65f9805
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-23 07:20:41 -0500

    removed unused files

--- openseed_html.py
+++ openseed_html.py
@@ -9,11 +9,6 @@ import time
 import openseed_core as Core
 from bottle import route, run, template, request, static_file
 
-settings = Settings.get_settings()
-
-
-
-
 @route('/api', method='POST')
 def index():
 	themessage = request.forms.get("msg")
