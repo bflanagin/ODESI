2b88b2748f505ef89f21c096333278c30fd1c16a
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-22 21:23:11 -0500

    backup, reverse, and back to the beginning

--- hive_get.py
+++ hive_get.py
@@ -112,7 +112,7 @@ def search_music(author,limit) :
 										img = metadata["audio"]["files"]["cover"]
 										genre = metadata["audio"]["genre"]
 										IPFS.pin_and_record(ipfs,artist,title,permlink,img,songtype,genre,songtags,duration)
-          									print("found audio "+title)
+										print("found audio "+title)
 	return(local)
 
 def search_history(user,limit):
