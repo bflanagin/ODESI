25824d89fd4e2d0789e7f80e237f7bb38db27b93
Benjamin Flanagin <bflanagin@gmail.com>
2020-04-29 21:07:37 -0500

    #60 work

--- openseed_seedgenerator.py
+++ openseed_seedgenerator.py
@@ -423,12 +423,12 @@ def simp_decrypt(key,raw_data):
 						message += chr(int(data[datanum]) + int(salt))
 				else:
 					split = 42
-					#if int(salt) % 2 == 0:
-					#	split = int(data[datanum]) - int(key_digits[keynum])
+					if int(salt) % 2 == 0:		
+						split = data[datanum] - key_digits[keynum]
 					#else:
 						#split = int(data[datanum]) / int(key_digits[keynum])
 						
-					message += chr(split)
+					message += split
 				datanum += 1
 			keynum += 1	
 			
