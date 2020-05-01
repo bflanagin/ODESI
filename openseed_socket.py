97d4c3bf517398b4c31bca1928e03301d1c67fb4
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-01 16:34:00 -0500

    fliping typo

--- openseed_socket.py
+++ openseed_socket.py
@@ -72,7 +72,8 @@ class TCPHandler(socketserver.BaseRequestHandler):
 			try:
 				json.loads(response)
 			except:
-				print("not json formated",response)
+				print("not json formated",response[0:10])
+				json.loads(response)
 			else:
 				chunky(response.strip(),self.request)
 
