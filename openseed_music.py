561b52e16959a4a7353511a0af978931d80f3938
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-01 17:02:42 -0500

    fixing outputs to pass json formating via python json library

--- openseed_music.py
+++ openseed_music.py
@@ -164,9 +164,9 @@ def get_genre_tracks(genre,count):
 	for genre in result:
 		if genre:
 			if output == "":
-				output ='{"author":"'+str(genre[0])+'","title":"'+str(genre[1])+'","post":"'+str(genre[2])+'","img":"'+str(genre[3])+'","ogg":"'+str(genre[4])+'","curation":"'+str(genre[5])+'","type":"'+str(genre[6])+'","genre":"'+str(genre[7])+'","tags":'+str(genre[8])+',"duration":"'+str(genre[9])+'"}'
+				output ='{"author":"'+genre[0]+'","title":"'+genre[1]+'","post":"'+genre[2]+'","img":"'+genre[3]+'","ogg":"'+genre[4]+'","curation":"'+str(genre[5])+'","type":"'+genre[6]+'","genre":"'+genre[7]+'","tags":'+genre[8]+',"duration":"'+str(genre[9])+'"}'
 			else:
-				output +=',{"author":"'+str(genre[0])+'","title":"'+str(genre[1])+'","post":"'+str(genre[2])+'","img":"'+str(genre[3])+'","ogg":"'+str(genre[4])+'","curation":"'+str(genre[5])+'","type":"'+str(genre[6])+'","genre":"'+str(genre[7])+'","tags":'+str(genre[8])+',"duration":"'+str(genre[9])+'"}'
+				output +=',{"author":"'+genre[0]+'","title":"'+genre[1]+'","post":"'+genre[2]+'","img":"'+genre[3]+'","ogg":"'+genre[4]+'","curation":"'+str(genre[5])+'","type":"'+genre[6]+'","genre":"'+genre[7]+'","tags":'+genre[8]+',"duration":"'+str(genre[9])+'"}'
 
 	music.close()
 	openseed.close()
