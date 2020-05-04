79fc5f752611df071bcd06485005d8478b15abd2
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-04 17:27:15 -0500

    trying slightly different output

--- openseed_music.py
+++ openseed_music.py
@@ -141,7 +141,7 @@ def get_genres():
 
 	music.close()
 	openseed.close()
-	return '{"genres":'+output+'}'
+	return '{"genres":'+str(output)+'}'
 
 def get_genre_totals(genre):
 	openseed = mysql.connector.connect(
