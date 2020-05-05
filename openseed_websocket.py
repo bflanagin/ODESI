69b8b0cb85c4a614670a75a7a53beb1727342b02
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-05 13:12:43 -0500

    updating websocket support

--- openseed_websocket.py
+++ openseed_websocket.py
@@ -2,11 +2,20 @@
 
 import asyncio
 import websockets
+import openseed_seedgenerator as Seed
+import openseed_account as Account
+import json
+import time
+import openseed_core as Core
+import subprocess
+import sys
+import os
 
 async def echo(websocket, path):
     async for message in websocket:
-        await websocket.send(message)
+    	response = Core.message(message)
+        await websocket.send(response)
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
+    websockets.serve(echo, '0.0.0.0', 8765))
 asyncio.get_event_loop().run_forever()
