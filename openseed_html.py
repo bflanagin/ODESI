eac8bfb358e18fde2e04f0cbdd55db1cf6eec43d
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-05 12:21:17 -0500

    Adding encryption to webserver

--- openseed_html.py
+++ openseed_html.py
@@ -7,13 +7,30 @@ sys.path.append("..")
 import json
 import time
 import openseed_core as Core
+import openseed_seedgenerator as Seed
 from bottle import route, run, template, request, static_file
 
 @route('/api', method='POST')
 def index():
 	themessage = request.forms.get("msg")
-	if themessage != None:
-		return Core.message(themessage)
+	appId = ""
+	key = ""
+	message = ""
+	if themessage.split("<::>") > 1:
+		appId = themessage.split("<::>")[0]
+		key = Account.get_priv_from_pub(appId,"App")
+		message = themessage.split("<::>")[1]
+		
+		decrypted = Seed.simp_decrypt(key,message)
+		
+		response = Core.message(decrypted)
+		
+		encrypt = Seed.simp_crypt(key,response)
+		
+		return encrypt
+	else:
+		if themessage != None:
+			return Core.message(themessage)
 
 
 run(host='0.0.0.0', port=8670)
