5b39dbe487f6675f87ed7bb49a4f54de68079664
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-05 19:13:18 -0500

    changing string to bytes

--- openseed_websocket.py
+++ openseed_websocket.py
@@ -11,10 +11,22 @@ import subprocess
 import sys
 import os
 
-async def echo(websocket, path):
+async def to_core(websocket, path):
 	async for message in websocket:
-		response = Core.message(message.decode())
-		await websocket.send(response)
+		themessage = message.decode()
+		if themessage.find("msg=") !=-1:
+			appId = self.data.decode().split("msg=")[1].split("<::>")[0]
+			key = Account.get_priv_from_pub(appId,"App")
+			message = self.data.decode().split("msg=")[1].split("<::>")[1]
+			if len(themessage.split("msg=")[1].split("<::>")) == 3:
+				decrypted = Seed.simp_decrypt(key,message)
+				response = Core.message(decrypted)
+				encrypt = Seed.simp_crypt(key,response)
+				await websocket.send(encrypt)
+			else:
+				print('{"server":"error incomplete message"}')
+				print(themessage)
+				await websocket.send(str('{"server":"error incomplete message"}').encode("utf8"))
 
-asyncio.get_event_loop().run_until_complete(websockets.serve(echo, '0.0.0.0', 8765))
+asyncio.get_event_loop().run_until_complete(websockets.serve(to_core, '0.0.0.0', 8765))
 asyncio.get_event_loop().run_forever()
