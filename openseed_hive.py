0c0a28215e17ccffd276ff82cc73517e93ba9852
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-07 21:52:11 -0500

    You would think I would know how to return the data properly by now

--- openseed_hive.py
+++ openseed_hive.py
@@ -61,7 +61,7 @@ title = "New Song"
 
 def get_post(author,permlink) :
 	post = h.get_content(author,permlink)
-	return '{"hive_post":{"author":"'+author+'","title":"'+json.dumps(post["title"])+'","post":"'+json.dumps(post["body"])+'"}}'
+	return '{"hive_post":{"author":"'+author+'","title":'+json.dumps(post["title"])+',"post":'+json.dumps(post["body"])+'}}'
 
 def get_account(account):
 	profile = '{"profile":"Not found"}'
