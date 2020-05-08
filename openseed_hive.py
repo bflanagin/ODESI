e66ddeea43e31f28337e501d09fd3f402b4bd360
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-07 21:25:30 -0500

    formating hive_post_return a little clearer

--- openseed_hive.py
+++ openseed_hive.py
@@ -61,7 +61,7 @@ title = "New Song"
 
 def get_post(author,permlink) :
 	post = h.get_content(author,permlink)
-	return '{"hive_post":"'+post["body"]+'"}'
+	return '{"hive_post":"author":"'+author+'","title":"'+post["title"]+'","post":"'+post["body"]+'"}'
 
 def get_account(account):
 	profile = '{"profile":"Not found"}'
