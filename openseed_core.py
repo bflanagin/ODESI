ee08063c4e6c33b86a7b9120d8f8ba03e34566af
Benjamin Flanagin <bflanagin@gmail.com>
2020-05-12 14:26:50 -0500

    need to read the documentation better

--- openseed_core.py
+++ openseed_core.py
@@ -109,7 +109,7 @@ def message(data):
 			elif action == "get_hive_post":
 				response = Hive.get_post(from_client["author"],from_client["permlink"])
 			elif action == "like_hive_post":
-				response = Hive.like_post(from_client["token"],from_client["post"],from_client["percent"])
+				response = Hive.like_post(from_client["token"],from_client["author"],from_client["post"],from_client["percent"])
 			
 			elif action == "hivecheck":
 				Account.hive_Check(from_client["hivename"])
